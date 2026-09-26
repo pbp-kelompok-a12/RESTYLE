91bbbb99b192fd922ad0aa4600a11eaacdc75a9c
fadlyatiefn <fadly.atief@ui.ac.id>
2026-09-26 17:29:34 +0700

    fix: fix middleware for pws deploy

--- restyle/settings.py
+++ restyle/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,6 +75,8 @@ TEMPLATES = [
 ]
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 WSGI_APPLICATION = 'restyle.wsgi.application'
 
