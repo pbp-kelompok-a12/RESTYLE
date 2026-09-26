0d2662572a1e3b7f78031041108af483a311fc9b
Salwa <salwa.hafiza@ui.ac.id>
2026-09-26 20:32:14 +0700

    Add mobile layout

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,22 @@
 Marquee3k.init();
+
+const navToggle = document.querySelector(".nav-toggle");
+const siteHeader = document.querySelector(".site-header");
+
+if (navToggle && siteHeader) {
+    navToggle.addEventListener("click", () => {
+        const isOpen = siteHeader.classList.toggle("nav-open");
+        navToggle.setAttribute("aria-expanded", isOpen);
+        navToggle.innerHTML = isOpen
+            ? '<i class="fa-solid fa-xmark"></i>'
+            : '<i class="fa-solid fa-bars"></i>';
+    });
+
+    document.querySelectorAll(".nav-act a").forEach((link) => {
+        link.addEventListener("click", () => {
+            siteHeader.classList.remove("nav-open");
+            navToggle.setAttribute("aria-expanded", "false");
+            navToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+        });
+    });
+}
